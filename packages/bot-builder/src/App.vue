f911e8d2629cef193e942296ef18de5002ded78a
m0ksem <m0ksem1337@gmail.com>
2021-12-10 23:01:49 +0200

    Fix drag.

--- packages/bot-builder/src/App.vue
+++ packages/bot-builder/src/App.vue
@@ -4,22 +4,29 @@ import DraggableCanvas from './components/DraggableCanvas.vue'
 
 const items = ref([
   {
-    data: 'Hello',
+    data: {
+      title: 'Message',
+      answer: '',
+    },
     position: { x: 0, y: 0 }
-  },
-    {
-    data: 'Hello',
-    position: { x: 0, y: 0 }
-  },
-    {
-    data: 'Hello',
-    position: { x: -10, y: 0 }
   },
 ])
 </script>
 
 <template>
   <DraggableCanvas v-model:items="items">
+    <template #item="{ data, listeners, style }">
+      <va-card>
+        <va-card-title v-on="listeners" :style="style">{{ data.title }}</va-card-title>
+        <va-card-content>
+          <va-input label="Answer" v-model="data.answer" />
+        </va-card-content>
+        <va-card-actions align="between">
+          <va-button>Action 1</va-button>
+          <va-button>Action 2</va-button>
+        </va-card-actions>
+      </va-card>
+    </template>
   </DraggableCanvas>
 </template>
 
--- packages/bot-builder/src/components/DraggableCanvas.vue
+++ packages/bot-builder/src/components/DraggableCanvas.vue
@@ -21,18 +21,18 @@ const { selected: selectedItem, set: selectItem, unset: unselectItem } = useSele
 const { selected: hoveredItem, set: hoverItem, unset: unHoverItem } = useSelected<Item>()
 
 const canvas = ref()
-const mouse = ref({ x: 0, y: 0 })
+const clickOffset = ref({ x: 0, y: 0 })
 const onMouseMove = (e: MouseEvent) => {
-  const { x, y, width, height } = canvas.value.getBoundingClientRect()
-
-  mouse.value = { 
-    x: e.clientX - x - (width / 2), 
-    y: e.clientY - y - (height / 2)
-  }
-
   if (!selectedItem.value) { return }
 
-  selectedItem.value.position =  { ...mouse.value }
+  const { x, y, width, height } = canvas.value.getBoundingClientRect()
+
+  const position = { 
+    x: e.x - x - (width / 2) - clickOffset.value.x,
+    y: e.y - y - (height / 2) - clickOffset.value.y
+  }
+
+  selectedItem.value.position = position
 }
 
 const getItemStyle = (item: Item) => {
@@ -40,6 +40,33 @@ const getItemStyle = (item: Item) => {
     transform: `translateX(${item.position.x}px) translateY(${item.position.y}px) translateX(-50%) translateY(-50%)`
   }
 }
+
+const onMouseDown = (e: MouseEvent) => {
+  const { x, y, width, height } = (e.target as HTMLElement).getBoundingClientRect()
+  clickOffset.value = { x: e.x - x - width / 2, y: e.y - y - height - height / 2 }
+
+  if (!hoveredItem.value) { return }
+
+  selectItem(hoveredItem.value);
+}
+
+const onMouseUp = () => { unselectItem(); }
+
+const createItemListeners = (item: Item) => {
+  let timer = -1
+
+  return {
+    mouseover: () => {
+      clearTimeout(timer)
+      hoverItem(item)
+    },
+    mouseout: () => {
+      timer = setTimeout(() => unHoverItem(), 1000)
+    },
+  }
+}
+
+const createItemStyle = () => { cursor: 'move' }
 
 watch(selectedItem, () => emit('update:selected', selectedItem.value))
 watch(hoverItem, () => emit('update:hovered', hoveredItem.value))
@@ -50,17 +77,16 @@ watch(hoverItem, () => emit('update:hovered', hoveredItem.value))
     class="draggable-canvas" 
     ref="canvas"
     @mousemove="onMouseMove"
+    @mousedown="onMouseDown"
+    @mouseup="onMouseUp"
+    @mouseout="unselectItem"
   >
     <div 
       class="draggable-canvas__item"
       v-for="(item, index) in computedItems" :key="index"
       :style="getItemStyle(item)"
-      @mousedown="selectItem(item)"
-      @mouseup="unselectItem()"
-      @mouseover="hoverItem(item)"
-      @mouseout="unHoverItem()"
     >
-      <slot name="item">
+      <slot name="item" v-bind="{ item, data: item.data, style: createItemStyle(), listeners: createItemListeners(item) }">
         <div class="draggable-canvas__item-content">
           {{ item.data }}
         </div>
@@ -73,8 +99,9 @@ watch(hoverItem, () => emit('update:hovered', hoveredItem.value))
   .draggable-canvas {
     position: relative;
     background: gray;
-    height: 50vh;
-    width: 50vw;
+    height: 50%;
+    width: 50%;
+    // overflow: hidden;
 
     &__item {
       cursor: pointer;
