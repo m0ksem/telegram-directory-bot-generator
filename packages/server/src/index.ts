404678ac3e63023daff57b709021ec47a04a5191
Olexandr <nadtochiy1915@gmail.com>
2021-12-16 00:40:18 +0200

    Added simple server template

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,20 @@
-import { initBot } from 'bot'
+import { config as loadDotEnv } from 'dotenv'
 
-const stopBot = initBot(process.env.BOT_TOKEN as string, { messages: [], settings: {} })
+loadDotEnv({ path: './.env.development' })
+const vetrybotToken = process.env.VETRYBOT_TOKEN;
 
-setTimeout(() => stopBot(), 1000)
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/vetrybot', (req, res) => {
+    if(vetrybotToken == req.body['token']){
+        
+        // Restart bot with new config: JSON.parse(req.body['config'])
+    }
+    res.sendStatus(200);
+});
+
+app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
