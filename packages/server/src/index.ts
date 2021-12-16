8229e3e404edcb42d298c952c8e47cfd4c60dbdf
Olexandr <nadtochiy1915@gmail.com>
2021-12-16 23:43:55 +0200

    raw

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,5 @@
 import { config as loadDotEnv } from 'dotenv'
+import { Bot } from 'bot'
 
 loadDotEnv({ path: './.env.development' })
 const vetrybotToken = process.env.VETRYBOT_TOKEN;
@@ -6,12 +7,17 @@ const vetrybotToken = process.env.VETRYBOT_TOKEN;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const configMessages = require('./config/messages.json');
+const configSettings = require('./config/settings.json');
+
+const bot = new Bot(process.env.BOT_TOKEN as string, { messages: configMessages, settings: configSettings })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/vetrybot', (req, res) => {
     if(vetrybotToken == req.body['token']){
         
+        bot.updateConfig({ messages: [], settings: bot.config.settings })
         // Restart bot with new config: JSON.parse(req.body['config'])
     }
     res.sendStatus(200);
