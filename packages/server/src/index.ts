b5fdd025d890f2f898281487b5c0c5f99ff9aadc
m0ksem <m0ksem1337@gmail.com>
2021-12-17 20:22:43 +0200

    Added build server command.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,8 @@
 import { Bot } from 'bot'
+import { n } from './utils/test'
 
 const bot = new Bot(process.env.BOT_TOKEN as string, { messages: [], settings: {} })
 
 bot.updateConfig({ messages: [], settings: {} })
+
+console.log(n)
